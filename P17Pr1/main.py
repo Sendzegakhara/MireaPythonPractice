aa769345a471848ab61e2b1a0203e55a2b9208f9
Sendzegakhara <sendzegakhara@mail.ru>
2023-01-01 10:28:39 +0300

    P17Pr0  Changes to be committed: 	new file:   .gitignore 	new file:   P17Pr1/main.py

---
+++ P17Pr1/main.py
@@ -0,0 +1,49 @@
+# TODO: Первая практика: интерфейс PyCharm, TODO, переменные,
+#  print, input, str(), int(), действия со строками и числами,
+#  разные ячейки памяти при n1="123" и n2=f"{123}"
+
+"""
+print("Hello world!")
+print(10 - 8 * 5 / 7 ** 2)
+
+name = "ABC"
+print(name)
+
+number = -1.42142
+print(number * number / number / 2 * 8 * number + number * 0.2)
+"""
+
+"""
+number1 = f"{123}"
+number2 = "123"
+number3 = 123
+print(type(number1))
+print(type(number2))
+print(type(number3))
+"""
+
+"""
+text = "cgw43g34cg34x_%@$HXX)!"
+text1 = "5122"
+number4 = 34
+print(text + str(number4))
+print(int(text1) + number4)
+print(number4 * text1)
+print("i" * 100)
+print(text + "2352352dfsdfsdf")
+number4.
+text.
+"""
+
+"""
+input("What's your name?")
+name = input()
+print(name.upper())
+"""
+
+"""
+text = input()
+number = int(text)
+print(type(text))
+print(type(number))
+"""
